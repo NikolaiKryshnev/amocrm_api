9b3e312319c887f168ab50cd37e467244f18204d
NikolaiKryshnev <63440682+NikolaiKryshnev@users.noreply.github.com>
2024-04-16 13:16:15 +0300

    Update interaction.py

--- amocrm/v2/interaction.py
+++ amocrm/v2/interaction.py
@@ -32,7 +32,7 @@ class BaseInteraction:
 		return {"Authorization": "Bearer " + self._token_manager.get_access_token()}
 
 	def _get_url(self, path):
-		logger.debag(f"path:{path}")
+		logging.debag(f"path:{path}")
 		return "https://{subdomain}.amocrm.ru/api/v4/{path}".format(subdomain=self._token_manager.subdomain, path=path)
 
 	def _request(self, method, path, data=None, params=None, headers=None):
@@ -92,9 +92,9 @@ class BaseInteraction:
 		return modified_data
 
 	def request(self, method, path, data=None, params=None, headers=None, include=None):
-		logger.debag(f"request - method: {method}")
-		logger.debag(f"request - path: {path}")
-		logger.debag(f"request - data: {data}")
+		logging.debag(f"request - method: {method}")
+		logging.debag(f"request - path: {path}")
+		logging.debag(f"request - data: {data}")
 		params = params or {}
 		if include:
 			params["with"] = ",".join(include)
